6a0d86a86ad7a43757e6ac810184c831450f19fd
Tom W Hartung <tomwhartung@gmail.com>
2015-08-07 14:49:55 -0600

    Adding "Hello World" version of jmws_idMyGadget_for_wordpress.php (which btw does not even print "Hello World" but at least it parses ok).

---
+++ wp-content/plugins/jmws_idMyGadget_for_wordpress/jmws_idMyGadget_for_wordpress.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * @package jmws_idMyGadget_for_wordpress
+ *
+ * Plugin Name: jmws_idMyGadget_for_wordpress
+ * Plugin URI: 
+ * Description: Integrate idMyGadget with a couple of wordpress themes (to start).
+ * Author: Tom Hartung
+ * Version: 1.0
+ * Author URI: http://tomwhartung.com/
+ */
+
+function jmws_idMyGadget_for_wordpress()
+{
+//	print 'Hello World from jmws_idMyGadget_for_wordpress.php .';
+}
+
+add_action( 'wp', 'jmws_idMyGadget_for_wordpress' );
+
