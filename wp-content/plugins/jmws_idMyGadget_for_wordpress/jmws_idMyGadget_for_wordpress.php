1bfcc689ba45f8d81b0c1d8c31032f239786bb85
Tom W Hartung <tomwhartung@gmail.com>
2015-08-10 21:12:38 -0600

    Added gadget_detectors_array and using it to get the values.

--- wp-content/plugins/jmws_idMyGadget_for_wordpress/jmws_idMyGadget_for_wordpress.php
+++ wp-content/plugins/jmws_idMyGadget_for_wordpress/jmws_idMyGadget_for_wordpress.php
@@ -12,19 +12,30 @@
 define( 'IDMYGADGET__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 require_once( IDMYGADGET__PLUGIN_DIR . '/JmwsIdMyGadgetWordpress.php' );
 // require_once 'jmws_idMyGadget_for_joomla/PhoneBurgerMenuIcon.php';
+
+/*
+ * Array of Gadget Detectors
+ */
+$gadget_detectors_array = array(
+	'detect_mobile_browsers',   // note that this is used as the default throughout
+	'mobile_detect',
+	'tera_wurfl'
+);
+
 $jmwsIdMyGadget = null;
 
 /**
- *
- * @global type $jmwsIdMyGadget
+ * @global array $gadget_detectors_array
+ * @global object $jmwsIdMyGadget
  */
 function jmws_idMyGadget_for_wordpress()
 {
+	global $gadget_detectors_array;
 //	print 'Hello World from jmws_idMyGadget_for_wordpress.php .';
 //	$gadgetDetector = $this->params->get('gadgetDetector');
-//	$gadgetDetector = 'detect_mobile_browsers';
-	$gadgetDetector = 'mobile_detect';
-//	$gadgetDetector = 'tera_wurfl';
+	$gadgetDetector = $gadget_detectors_array[0];
+//	$gadgetDetector = $gadget_detectors_array[1];
+//	$gadgetDetector = $gadget_detectors_array[2];
 	global $jmwsIdMyGadget;
 	$jmwsIdMyGadget = new JmwsIdMyGadgetWordpress($gadgetDetector);
 
