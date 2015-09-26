19140b8d1ef2f4ce322bb2eb3acc56b51a84521d
Tom W Hartung <tomwhartung@gmail.com>
2015-09-26 17:03:51 -0600

    Do't panic it is only some very minor whitespace cleanup.

--- wp-content/plugins/idMyGadget/idMyGadgetOptionsPage.php
+++ wp-content/plugins/idMyGadget/idMyGadgetOptionsPage.php
@@ -17,10 +17,8 @@ function idMyGadget_options_page_html_fcn()
 	print '<h2>IdMyGadget Options</h2>';
 	print '<p>Device-specific options for use by themes that know what to do with them.</p>';
 	print '<form action="options.php" method="post">';
-
 	settings_fields( 'idMyGadget_option_settings' );
 	do_settings_sections( 'idMyGadget_option_settings' );
 	submit_button();
 	print '</form></div>';
 }
-
